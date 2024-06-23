fac6e8446770c4905e2494d2ab9fb99aca43116e
gitgirlpragya <pragyathakur2003@gmail.com>
2024-06-24 00:25:05 +0530

    Add PrimeNumber.java program

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int number = 29; // You can change this number to test other values
+        boolean isPrime = true;
+
+        for (int i = 2; i <= number / 2; ++i) {
+            // condition for nonprime number
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            System.out.println(number + " is a prime number.");
+        else
+            System.out.println(number + " is not a prime number.");
+    }
+}
